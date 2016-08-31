a0151b2df9997477c9d9c689a09419f529178840
Edwin Carbajal & Ephraim Friedman & Matthew Kubota <pair+edwincarbajal.ephraimfriedman.mattk5@devbootcamp.com>
2016-08-31 12:21:17 -0400

    finished associations

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,3 +1,7 @@
 class Answer < ActiveRecord::Base
-  # Remember to create a migration!
+  belongs_to :author, class_name: "User"
+  belongs_to :question
+
+  has_many :comments, :as => :commentable
+  has_many :votes, :as => :votable
 end
