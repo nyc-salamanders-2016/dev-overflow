9fd9161bc9400bd38c8e8c2023f461371bd3b7ff
Ephraim Friedman <ephraimfried@gmail.com>
2016-08-31 17:55:32 -0400

    add feature to create comment for question and answer and redirects to question thread (not merged yet)

---
+++ app/controllers/comment.rb
@@ -0,0 +1,43 @@
+
+get '/questions/:id/comments/new' do
+  @question = Question.find(params[:id])
+
+  erb :'comments/new'
+end
+
+post '/questions/:id/comments' do
+
+  question = Question.find(params[:id])
+  comment = question.comments.build(body: params[:comment_body], author: current_user)
+
+  if comment.save
+
+    redirect "/questions/#{params[:id]}"
+  else
+     @errors = ["inappropriate comment"]
+     erb :'comments/new'
+  end
+end
+
+get '/answers/:id/comments/new' do
+
+  @answer = Answer.find(params[:id])
+
+  erb :'comments/new_comment_for_answer'
+end
+
+post '/answers/:id/comments' do
+
+  answer = Answer.find(params[:id])
+  comment = answer.comments.build(body: params[:comment_body], author: current_user)
+
+  if comment.save
+    question_id = answer.question.id
+    redirect "/questions/#{question_id}"
+  else
+     @errors = ["inappropriate comment"]
+     erb :'comments/new'
+  end
+end
+
+
