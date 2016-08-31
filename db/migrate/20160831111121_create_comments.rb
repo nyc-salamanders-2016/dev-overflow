50552f391d44a219876bc7275ca847dc13955f32
Edwin Carbajal & Ephraim Friedman & Matthew Kubota <pair+edwincarbajal.ephraimfriedman.mattk5@devbootcamp.com>
2016-08-31 11:39:12 -0400

    create the rest of the tables

--- db/migrate/20160831111121_create_comments.rb
+++ db/migrate/20160831111121_create_comments.rb
@@ -1,4 +1,11 @@
 class CreateComments < ActiveRecord::Migration
   def change
+    create_table :comments do |t|
+      t.string :body
+      t.references :user
+      t.references :commentable, polymorphic: true
+      
+      t.timestamps(null: false)
+    end
   end
 end
--- db/migrate/20160831111207_create_questions_tags.rb
+++ db/migrate/20160831111207_create_questions_tags.rb
@@ -1,4 +1,9 @@
 class CreateQuestionsTags < ActiveRecord::Migration
   def change
+    create_table :questions_tags do |t|
+      t.references :question, :tag
+
+      t.timestamps(null: false)
+    end
   end
 end
