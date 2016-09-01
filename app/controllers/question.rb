aecc1f7f7a215e3d29714491d593b89e3a0863f4
Matthew Kubota <email": null>
2016-09-01 10:14:43 -0400

    added method to sessions file.

---
+++ app/controllers/question.rb
@@ -0,0 +1,47 @@
+get '/questions/new' do
+  logged_in?
+  erb :'/questions/new'
+end
+
+post '/questions' do
+  @question = Question.new(params[:question])
+    if @question.save
+      redirect '/questions/index'
+    else
+      @errors = @question.errors.full_messages
+      erb :'/questions/new'
+    end
+end
+
+get '/questions' do
+  @questions = Question.all
+  erb :'/questions/index'
+end
+
+get '/questions/:id' do
+  @question = Question.find(params[:id])
+  @anwer = Answer.find_by(params[:user_id])
+  erb :'/questions/show'
+end
+
+get '/questions/:id/edit' do
+  @question = Question.find(params[:id])
+  erb :'/questions/edit'
+end
+
+put '/questions/:id' do
+  @question = Question.find(params[:id])
+  @question.assign_attributes(params[:question])
+  if @question.save
+    redirect '/questions'
+  else
+    erb :'/questions/edit'
+  end
+
+end
+
+delete '/questions/:id' do
+  @question = Question.find_by(params[:id])
+  @question.destroy
+  redirect '/questions'
+end
--- app/helpers/session.rb
+++ app/helpers/session.rb
@@ -1,3 +1,7 @@
 def current_user
   @current_user ||= User.find_by(id: session[:id])
 end
+
+def logged_in?
+  redirect '/users/login' unless current_user
+end
