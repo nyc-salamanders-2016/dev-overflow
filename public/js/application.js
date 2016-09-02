3652fc53c1038fb1cc57b5254b488217ad8ec737
Edwin Carbajal <email": null>
2016-09-01 22:56:33 -0400

    fixed

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,50 @@
 $(document).ready(function() {
+    $('.upvote-question').submit(function(event) {
+      event.preventDefault();
 
+      $.ajax({
+        method: 'POST',
+        url: $(event.target).attr('action')
+      })
+        .done(function (response) {
+          $(event.target).next().text(response);
+        });
+    });
+
+    $('.downvote-question').submit(function (event) {
+      event.preventDefault();
+
+      $.ajax({
+        method: 'POST',
+        url: $(event.target).attr('action')
+      })
+        .done(function(response) {
+          $(event.target).prev().text(response);
+        });
+    });
+
+    $('.upvote-answer').submit(function(event) {
+      event.preventDefault();
+
+      $.ajax({
+        method: 'POST',
+        url: $(event.target).attr('action')
+      })
+        .done(function (response) {
+          // debugger;
+          $(event.target).next().text(response);
+        });
+    });
+
+    $('.downvote-answer').submit(function (event) {
+      event.preventDefault();
+
+      $.ajax({
+        method: 'POST',
+        url: $(event.target).attr('action')
+      })
+        .done(function (response) {
+          
+        })
+    });
 });
